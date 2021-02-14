d91e14fb679513b06f03e6532874466454c8ec9c
Shanmuga Devi <shanvalar059@gmail.com>
2021-02-15 03:40:03 +0530

    commit

---
+++ list/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):  # add this
+    list_display = ('title', 'description', 'completed')  # add this
+
+
+admin.site.register(Todo, TodoAdmin)  # add this
